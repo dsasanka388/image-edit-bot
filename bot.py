433a2e12530c68a454541f16213363ee5adc7448
dsasanka388 <dsasanka388@gmail.com>
2026-07-12 13:51:07 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -39,7 +39,32 @@ async def save_photo(client, message: Message):
     await message.reply_text(
         "✅ Photo saved successfully!"
     )
+from PIL import Image
 
+@app.on_message(filters.command("crop"))
+async def crop_image(client, message: Message):
+    user_id = message.from_user.id
+
+    if user_id not in user_images:
+        return await message.reply_text("❌ Pehle photo bhejo.")
+
+    input_file = user_images[user_id]
+    output_file = f"{DOWNLOAD_PATH}/{user_id}_crop.jpg"
+
+    img = Image.open(input_file)
+
+    width, height = img.size
+    size = min(width, height)
+
+    left = (width - size) // 2
+    top = (height - size) // 2
+    right = left + size
+    bottom = top + size
+
+    img = img.crop((left, top, right, bottom))
+    img.save(output_file)
+
+    await message.reply_photo(output_file)
 
 if __name__ == "__main__":
     print("Bot Started...")
